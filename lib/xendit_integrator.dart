4ccc031c1002168329f2a9b0cf83e29bcb52a8b2
Mohamad Radja Qur'ani <125847269+mradjaq-paper@users.noreply.github.com>
2023-09-17 16:16:21 +0700

    first test

---
+++ lib/xendit_integrator.dart
@@ -0,0 +1,32 @@
+library xendit_integrator;
+
+import 'package:flutter/material.dart';
+
+class RadjaCustomButton extends StatelessWidget {
+  RadjaCustomButton({Key? key, required this.onPressed, required this.child})
+      : super(key: key);
+  final VoidCallback onPressed;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(16.0),
+          primary: Colors.white,
+          backgroundColor: Colors.blue,
+          elevation: 9.0,
+          textStyle: const TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        child: child);
+  }
+}
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
